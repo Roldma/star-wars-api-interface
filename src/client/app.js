077f725b0027f304e159c2b1ee32a8e63e02c175
roldma <roldma@farmingdale.edu>
2018-09-26 16:02:06 -0400

    added redux and approuter, updated dependencies

---
+++ src/client/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/styles.css';
+import AppRouter from './router/AppRouter';
+
+const App = () => {
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>;
+};
+
+export default App;
