08322541bbacfe746271ba489d897d911fe0b745
roldma <roldma@farmingdale.edu>
2018-09-26 13:55:26 -0400

    file structure setup, dependencies added

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'client'),
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/build/',
+    filename: 'webpack-bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
