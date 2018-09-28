5ed5af04632184206449be4b738f764997026cda
roldma <roldma@farmingdale.edu>
2018-09-28 17:24:12 -0400

    changed to presentational comp

--- src/client/components/characterList.jsx
+++ src/client/components/characterList.jsx
@@ -4,22 +4,14 @@ import { connect } from 'react-redux';
 const mapStateToProps = store => ({});
 const mapDispatchToProps = dispatch => ({});
 
-class CharList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="charlist">
-        <ul>
-          <li>item1</li>
-          <li>item2</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const CharList = () => (
+  <div className="charlist">
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+    </ul>
+  </div>
+);
 
 export default connect(
   mapStateToProps,
