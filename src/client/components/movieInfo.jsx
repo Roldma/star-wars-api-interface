911fe65cebd77f52c8030978866fe19b133e2c64
budken <siu.afmm@gmail.com>
2018-10-01 21:14:53 -0400

    Displaying char info and separated films

--- src/client/components/movieInfo.jsx
+++ src/client/components/movieInfo.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-const movieInfo = (props) => (
-  
-)
+const MovieInfo = props => {
+  // console.log(props);
+  const { state } = props;
+  console.log('this is the selected Films Initially: ', state.selectedFilms);
+
+  const info = Object.entries(state.chars[state.selectedChar].info).map(
+    ([key, value], index) => {
+      if (key !== 'films') {
+        return <div key={`${key} ${index}`}>{`${key}: ${value} `} </div>;
+      }
+    }
+  );
+  const films = state.selectedFilms.map((value, index) => {
+    return <div key={`${value} ${index}`}>{value}</div>;
+  });
+  // console.log('films: ', films);
+
+  return (
+    <div>
+      <div>{info}</div>
+      <br />
+      <div>
+        Films with this Character: <br /> {films}
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
