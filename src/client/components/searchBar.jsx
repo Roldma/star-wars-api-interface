bbd8d3a105c2cdfc1855482842201ef5df1b8147
roldma <roldma@farmingdale.edu>
2018-10-26 22:52:55 -0400

    added searchBar component to be used in characterlist

---
+++ src/client/components/searchBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class searchBar extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleSubmit() {}
+}
