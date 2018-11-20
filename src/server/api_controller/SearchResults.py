934f0707f87359720e3f06bc981246d80143d25e
roldma <roldma@farmingdale.edu>
2018-11-20 17:01:49 -0500

    refactored SearchResults to use property decorator (set/get)

--- src/server/api_controller/SearchResults.py
+++ src/server/api_controller/SearchResults.py
@@ -1,4 +1,4 @@
-"""Interacts with SWAPI to get search query results"""
+"""SearchResults Instantiated which contains response from SWAPI (in member results)"""
 
 import requests
 from flask import jsonify
@@ -9,26 +9,33 @@ class SearchResults:
     Parameters
     ----------
     query: tuple 
-           First element is the category from radio button, Second element is the user input string from search bar
+       First element is the category, Second element is the user input string
     """
 
-    def __init__(self, query):
-        self.results = self.get_request(query)
+    def __init__(self, query, results=400):
+        self._query = query
+        self.results = results
 
-    def get_request(self, query):
-        swapi_search_url = "https://swapi.co/api/{}/?search={}".format(
-            query[0], query[1]
-        )
+    @property
+    def results(self):
+        return self._results
 
-        response_data = requests.get(swapi_search_url)
-        jsoned_response = jsonify(response_data.json())
+    @results.setter
+    def results(self, value):
+        def get_request(query):
+            swapi_search_url = "https://swapi.co/api/{}/?search={}".format(
+                query[0], query[1]
+            )
+            response_data = requests.get(swapi_search_url)
+            jsoned_response = jsonify(response_data.json())
 
-        return jsoned_response
+            return jsoned_response
+
+        value = get_request(self._query)
+        self._results = value
 
     def __repr__(self):
-        return "{} These are results produced by make_request method".format(
-            self.results
-        )
+        return "{} Results return from SWAPI".format(self.results)
 
 
 def create_search_results(query):
