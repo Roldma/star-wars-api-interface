96065645a4fcf66f1f75e3fae4b4ca9a668f0948
roldma <roldma@farmingdale.edu>
2018-11-21 12:23:48 -0500

    database connection created, global to prevent multiple connections

--- src/server/db_controller/redis_instance.py
+++ src/server/db_controller/redis_instance.py
@@ -1,9 +1,25 @@
 import redis
 
-db = redis.Redis(host="localhost", port=6379, db=0)
+# db = redis.Redis(host="localhost", port=6379, db=0)
+
+redis_pool = None
 
 
-def db_test():
-    db.set("foo", "chunks")
-    print(db.get("foo"))
-    pass
+def initConn():
+    global redis_pool
+    redis_pool = redis.ConnectionPool(host="localhost", port=6379, db=0)
+
+
+class RedisConnection:
+    def __init__(self):
+        self.redis_conn = redis.Redis(connection_pool=redis_pool)
+
+    def update_recent_search(self, search_str):
+        self.redis_conn.sadd("recent:search", search_str)
+
+
+def redis_conn():
+    initConn()
+    db = RedisConnection()
+    return db
+
