c6c1eaef3ca816bd28370117b05e55b03c125b0e
roldma <roldma@farmingdale.edu>
2018-11-28 15:53:04 -0500

    module to create/update recent search list for response

---
+++ src/server/api/recent_search.py
@@ -0,0 +1,72 @@
+"""
+RecentSearch : Class used for creating recent search list, and updating
+get_recent() : function returning a recent search list from redis db (instantiated from RecentSearch)
+update_recent(arg:string) : function updating recent search list in redis db
+"""
+
+import dbcontroller
+from dbcontroller import redis_instance
+from flask import jsonify
+
+
+class RecentSearch:
+    """
+    Class used to create recent search list, and update recent search list
+    
+    Parameters
+    ----------
+    rconn: redis instance/connection
+        connection to the redis db
+    query: string
+        search query string from user input
+    """
+
+    def __init__(self, rconn, query=None):
+        self._query = query
+        self._rconn = rconn
+        self._rset_key = "recent:search"
+
+    @property
+    def recent_searches(self):
+        return self._recent_searches
+
+    @recent_searches.setter
+    def recent_searches(self, value):
+        rset = self._rconn.smembers(self._rset_key)
+
+        search_list = [
+            self._decoder(member) for member in rset if len(self._decoder(member)) > 0
+        ]
+
+        value = jsonify(search_list)
+        self._recent_searches = value
+
+    def update_recent_searches(self):
+        self._rconn.sadd(self._rset_key, self._query)
+        print("Query added to set")
+
+    def _decoder(self, item):
+        return item.decode("utf-8")
+
+
+def _rconn():
+    """Returns an instance/connection to the redis db"""
+    return redis_instance.create_rconn()
+
+
+def get_recent():
+    """Retrieves recent search list from redis db"""
+    return RecentSearch(_rconn()).recent_searches
+
+
+def update_recent(query):
+    """
+    Updates recent:search key in redis database
+    
+    Parameters
+    ----------
+    query: string
+        string from user input search
+    """
+
+    RecentSearch(_rconn(), query).update_recent_searches()
