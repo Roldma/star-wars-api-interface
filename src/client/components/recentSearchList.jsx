a9f349431492ba3f2f400ec02903dbf6b981fecc
roldma <roldma@farmingdale.edu>
2018-11-28 01:16:15 -0500

    port changes, get request for redis recent search list stored in searchPage state, passed down as props to recentSearchList

--- src/client/components/recentSearchList.jsx
+++ src/client/components/recentSearchList.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const RecentSearch = (props) => {
+const RecentSearchList = (props) => {
   const { searchList } = props;
-  return <div>TEST</div>;
+  console.log(searchList);
+  let uniqueId = 0;
+
+  const recentList = searchList.map((str) => {
+    uniqueId += 1;
+    return <li key={`rSearch${uniqueId}`}>{str}</li>;
+  });
+
+  return (
+    <div>
+      <ul>{recentList}</ul>
+    </div>
+  );
 };
 
-export default RecentSearch;
+export default RecentSearchList;
