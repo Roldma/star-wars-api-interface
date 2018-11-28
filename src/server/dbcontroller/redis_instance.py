d3b8b69e38ef02eab66e20d2a8a9b545ac09686b
roldma <roldma@farmingdale.edu>
2018-11-28 15:53:54 -0500

    removed recent search functionality

--- src/server/dbcontroller/redis_instance.py
+++ src/server/dbcontroller/redis_instance.py
@@ -11,33 +11,11 @@ def initConn():
 
 class RedisConnection:
     def __init__(self, recent_search_list=None):
-        self._redis_conn = redis.Redis(connection_pool=redis_pool)
-        self.recent_search_list = recent_search_list
-
-    @property
-    def recent_search_list(self):
-        return self._recent_search_list
-
-    @recent_search_list.setter
-    def recent_search_list(self, value):
-        r_set_name = "recent:search"
-        r_set_members = self._redis_conn.smembers(r_set_name)
-
-        search_list = [
-            self._decoder(member)
-            for member in r_set_members
-            if len(self._decoder(member)) > 0
-        ]
-
-        value = jsonify(search_list)
-        self._recent_search_list = value
-
-    def _decoder(self, item):
-        return item.decode("utf-8")
+        self.redis_conn = redis.Redis(connection_pool=redis_pool)
 
 
-def redis_conn():
+def create_rconn():
     initConn()
-    db = RedisConnection()
+    db = RedisConnection().redis_conn
     return db
 
