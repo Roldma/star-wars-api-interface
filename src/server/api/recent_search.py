8b9cc21853a26db3f4b1103d52116ea311c15d47
roldma <roldma@farmingdale.edu>
2018-12-12 20:19:10 -0500

    WIP search controller

--- src/server/api/recent_search.py
+++ src/server/api/recent_search.py
@@ -1,16 +1,19 @@
 """
-RecentSearchControl : Class used for creating recent search list, and updating
+RecentSearch : Class used for creating recent search list, and updating
 get_recent() : function returning a recent search list from redis db (instantiated from RecentSearchControl)
 update_recent(arg:string) : function updating recent search list in redis db
 clear_recent() : function deleting recent searches key in redis DB
 """
 
 import dbcontroller
+import search_response
+
+from search_response import BasicSearchItem
 from dbcontroller.redis_instance import create_rconn
 from flask import jsonify
 
 
-class RecentSearchControl:
+class RecentSearch:
     """
     Class used to create recent search list, and update recent search list
     
@@ -22,9 +25,9 @@ class RecentSearchControl:
         search query string from user input
     """
 
-    def __init__(self, rconn, query=None, recent_searches=None):
+    def __init__(self, query=None, recent_searches=None):
         self._query = query
-        self._rconn = rconn
+        self._rconn = create_rconn()
         self._rset_key = "recent:search"
         self.recent_searches = recent_searches
 
@@ -48,7 +51,6 @@ class RecentSearchControl:
     def clear_recent_searches(self):
         self._rconn.delete(self._rset_key)
         print("Successfully deleted Key")
-        pass
 
     def _parse_recent(self):
         rset = self._rconn.smembers(self._rset_key)
@@ -64,12 +66,62 @@ class RecentSearchControl:
         return item.decode("utf-8")
 
 
+class SearchController:
+    def __init__(self, query=None, search_type=None, action=None, control_type=None):
+        self._query = query
+        self._search_type = search_type
+        self._action = action
+        self._control_type = control_type
+        # NEED TO USE ARRAY OF ARGS?? need to figure out how to deal with out of position arguments
+
+    @property
+    def _control_type(self):
+        return self.__control_type
+
+    @_control_type.setter
+    def _control_type(self, val):
+        if val == "recent":
+            if not self._query:
+                self.__control_type = RecentSearch()
+            else:
+                self.__control_type = RecentSearch(self._query)
+
+        elif val == "basic":
+            self.__control_type = BasicSearchItem(self._query)
+
+    def get_results(self):
+        if self._search_type == "recent":
+            results = self._control_type.recent_searches
+
+        elif self._search_type == "basic":
+            basic_search = BasicSearchItem(self._query)
+            results = basic_search.results
+
+        return results
+
+    def update_recent(self):
+        try:
+            rec_control = RecentSearch(self._query)
+            rec_control.update_recent_searches()
+        except:
+            return "Error updating recent list"
+
+    def clear_recent(self):
+
+        pass
+
+
 def create_controller(query=None):
     if query:
-        controller = RecentSearchControl(create_rconn(), query)
+        controller = RecentSearch(query)
     elif not query:
-        controller = RecentSearchControl(create_rconn())
-
+        controller = RecentSearch()
+    # try:
+    #     search_type = query["search_type"]
+    # except KeyError:
+    #     return 'Key "search_type" required in query object'
+    # r_conn = create_rconn()
+    # controller = SearchController(r_conn, query, search_type)
     return controller
 
 
